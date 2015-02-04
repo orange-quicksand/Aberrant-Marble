382c92d893ddad2ec6b65fa26da625a0cdac05a2
Mike <realmike33@gmail.com>
2015-02-03 22:05:46 -0800

    (cleanup) removed database, passport and fb login. made basic express server to serve client assests

--- db/schema.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE language;
-
-USE language;
--- server/server.js
+++ server/server.js
@@ -1,50 +1,23 @@
-var express = require('express');
-// var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var express = require('express');
 var app = express();
-var crypto = require('crypto');
-var session = require('express-session');
 
-// config file to instantiate 
-var User = require('./server/user/userController');
-var Users = require('./db/index');
 // config file to instantiate all queues
-var queues = require('./server/queue/queueCollection.js');
-var queueModel = require('./server/queue/queueModel.js');
-
-var passport = require('./server/facebookSignin.js');
+var queues = require('./queue/queueCollection.js');
+var queueModel = require('./queue/queueModel.js');
 
 var port = process.env.PORT || 3000;
 var host = process.env.host || '127.0.0.1';
 
-
-app.use(express.static(__dirname));
-// app.use(express.cookieParser('shhhh, very secret'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true
-}));
-
+app.use(express.static(__dirname + '/../client/'));
 
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
 
-// app.use(partials());
-
-
-app.set('views', __dirname);
-
 module.exports = app;
-
-app.get('/', function(request, response) {
-  response.redirect('./index.html');
-});
 
 //when a user clicks his native and desired language and clicks go, send a post request to api/languages
 //create a queue for that specific language queue, then 
@@ -77,31 +50,8 @@ app.get('/api/getroom', function(request, response) {
 
 app.get('/api/position', function(req, res) {
   var room = req.query.room;
-
-  
-
   // response.s 
-
-
   var room = req.query.room;
   // for(var i = 0;)
 
 });
-
-app.get('/signup', User.signUpUserForm);
-app.post('/signup', User.signUpUser);
-
-app.get('/signin', User.signInUserForm);
-app.post('/signin', User.signInUser);
-
-
-//Passport facebook auth
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_birthday', 'user_likes'] }));
-
-// Facebook will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-app.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/login' }));
