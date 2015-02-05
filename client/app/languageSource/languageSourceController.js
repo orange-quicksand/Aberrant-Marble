647279bb3f2059e7460c7de5824894473dafa514
Mike <realmike33@gmail.com>
2015-02-05 15:52:08 -0800

    (feat) Add more styling to index page and language 2 page

--- client/app/languageSource/languageSourceController.js
+++ client/app/languageSource/languageSourceController.js
@@ -3,7 +3,6 @@ angular.module('uSpeak.languageSource', [])
 .controller('languageSourceController', function($scope, languageService, $state){
   $scope.languages = [['English','us'],['Chinese','cn'],['Spanish','es'],['French','fr'],['Italian','it']];
   $scope.test = function(lang){
-  	console.log('working');
     languageService.language.source = lang[0];
     $state.go('languageTarget');
   };
