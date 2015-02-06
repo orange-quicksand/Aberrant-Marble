48fb6a7bff3e33056fe1d836958cc29141f9d242
Ryan <ryanjlee12@gmail.com>
2015-02-05 18:50:49 -0800

    (Feat) Fix formatting for home buttons

--- client/app/language1/language1Controller.js
+++ client/app/language1/language1Controller.js
@@ -3,6 +3,7 @@ angular.module('uSpeak.language1', [])
 .controller('language1Controller', function($scope, languageService, $state){
   $scope.languages = [['English','us'],['Chinese','cn'],['Spanish','es'],['French','fr'],['Italian','it']];
   $scope.test = function(lang){
+  	console.log('working');
     languageService.language.source = lang[0];
     $state.go('language2');
   };
