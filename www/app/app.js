7b611b2d062fc76bec8bf045cdd53038d48e1c20
Ryan <ryanjlee12@gmail.com>
2015-02-06 16:10:17 -0800

    (Feat) Implement index.html and language source with ionic

---
+++ www/app/app.js
@@ -0,0 +1,36 @@
+var app = angular.module('uSpeak', [ 
+  'ionic',
+  'ui.router',
+  'uSpeak.languageSource',
+  'uSpeak.languageTarget',
+  'uSpeak.loading',
+  'uSpeak.chat'
+  ])
+
+.controller('toolbarController', function(){
+})
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('languageSource', {
+      url: '/',
+      templateUrl: 'app/languageSource/languageSourceTemplate.html',
+      controller: 'languageSourceController'
+    })
+    .state('languageTarget', {
+      url: '/target',
+      templateUrl: 'app/languageTarget/languageTargetTemplate.html',
+      controller: 'languageTargetController'
+    })
+    .state('loading', {
+      url: '/loading',
+      templateUrl: 'app/loading/loadingTemplate.html',
+      controller: 'loadingController'
+    })
+    .state('chat', {
+      url: '/chat',
+      templateUrl: 'app/chat/chatTemplate.html',
+      controller: 'chatController'
+    });
+  $urlRouterProvider.otherwise("/");
+});
